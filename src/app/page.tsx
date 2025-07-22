382b8fb836d2673f58c976c16dce861ea52c9765
Adam Gray <adamgray@mac.home>
2025-07-22 23:17:02 +0100

    feat: add Clerk auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,36 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+import { AppSidebarClient } from "./_components/app-sidebar-client"
+
 export default function HomePage() {
-  return <h1>Hello World!</h1>
+  return (
+    <SidebarProvider className="overflow-y-hidden">
+      <AppSidebarClient>
+        <Sidebar collapsible="icon" className="overflow-hidden">
+          <SidebarHeader className="flex-row">
+            <SidebarTrigger />
+            <span className="text-xl  text-nowrap">WDS Jobs</span>
+          </SidebarHeader>
+          <SidebarContent>sdg</SidebarContent>
+          <SidebarFooter>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton>dsf</SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarFooter>
+        </Sidebar>
+        <main className="flex-1"></main>
+      </AppSidebarClient>
+    </SidebarProvider>
+  )
 }
